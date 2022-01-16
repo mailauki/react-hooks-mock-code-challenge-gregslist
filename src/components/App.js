b85ed503efc84f1ad868abf84a2a507e2d9d9f75
Julie Evans <juliemevans17@gmail.com>
2022-01-15 20:32:00 -0600

    commit tree b09011a49a5c12dcb464ea58eda10556f7b1db78

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(r => r.json())
+    .then(data => setListings(data))
+  }, [])
+
+  function handleDelete(deletedListing) {
+    const updatedListings = listings.filter(listing => {
+      if(listing.id !== deletedListing.id) return listing
+    })
+    console.log(deletedListing)
+    setListings(updatedListings)
+  }
+
+  const listingsToDisplay = listings.filter(listing => {
+    if(listing.description.toLowerCase().includes(search.toLowerCase())) 
+    return listing
+  })
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={searchedValue => setSearch(searchedValue)} />
+      <ListingsContainer listings={listingsToDisplay} onDeleteListing={handleDelete} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({ onSearchSubmit }) {
+  const [searchedValue, setSearchedValue] = useState("")
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    onSearchSubmit(searchedValue);
+    setSearchedValue("")
   }
 
   return (
@@ -12,8 +15,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={searchedValue}
+        onChange={(e) => setSearchedValue(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
